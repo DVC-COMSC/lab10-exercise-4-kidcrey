eba8039448b091dd3e1490d4f89c55649465b0b3
creyes <109313036+creyes027@users.noreply.github.com>
2023-03-02 18:58:35 +0000

    I fixed the main.py file and complete exercise 4

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
+    celcius = float(input('Enter a temperature in         Celcius: '))
+
+    multiply = 9/5
+
+    addition = 32
+
+
+    farenheit = (celcius*multiply)+addition
+
+    print(f'Farenheit: {farenheit :.2f}')
     pass
 
 
